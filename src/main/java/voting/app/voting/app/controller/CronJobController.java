7063cf5f0872b6f3454743b00b4163d1c2bc49d2
mkliac <90816129+mkliac@users.noreply.github.com>
2024-01-14 15:47:09 +0800

    add CronJobController

---
+++ src/main/java/voting/app/voting/app/controller/CronJobController.java
@@ -0,0 +1,22 @@
+package voting.app.voting.app.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+//dummy controller for cronjob to activate server hosted on render
+@RestController
+@RequestMapping("/")
+@Slf4j
+public class CronJobController {
+    @GetMapping
+    public ResponseEntity<String> dummy() {
+        log.info("CronJob triggered at {}", LocalDateTime.now());
+        return new ResponseEntity<>("success", HttpStatus.OK);
+    }
+}
