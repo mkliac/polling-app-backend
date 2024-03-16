c1c08a6659a5d41635355b9cc73fa8c2d4bc2cfd
mkliac <90816129+mkliac@users.noreply.github.com>
2024-03-16 18:41:02 +0800

    fix error message

--- src/main/java/voting/app/voting/app/dto/SavePollRequest.java
+++ src/main/java/voting/app/voting/app/dto/SavePollRequest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Data
 public class SavePollRequest {
-    @NotEmpty()
+    @NotEmpty(message = "Title is required")
     @Schema(description = "Title of the poll", required = true)
     private String title;
 
