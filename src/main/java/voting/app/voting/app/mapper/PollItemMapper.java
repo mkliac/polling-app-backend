8a4002cdf37859c2ee7b4f6bc5486cc6b375c307
mkliac <90816129+mkliac@users.noreply.github.com>
2024-04-05 13:20:00 +0800

    remove redundant wired bean

--- src/main/java/voting/app/voting/app/mapper/PollItemMapper.java
+++ src/main/java/voting/app/voting/app/mapper/PollItemMapper.java
@@ -3,16 +3,13 @@ package voting.app.voting.app.mapper;
 import java.util.List;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import voting.app.voting.app.dto.poll.PollItemDto;
 import voting.app.voting.app.model.poll.PollItem;
-import voting.app.voting.app.repository.VoteRepository;
 
 @Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
-public abstract class PollItemMapper {
-    @Autowired private VoteRepository voteRepository;
+public interface PollItemMapper {
 
-    public abstract PollItemDto toPollItemDto(PollItem pollItem);
+    PollItemDto toPollItemDto(PollItem pollItem);
 
-    public abstract List<PollItemDto> toPollItemDtos(List<PollItem> pollItems);
+    List<PollItemDto> toPollItemDtos(List<PollItem> pollItems);
 }
